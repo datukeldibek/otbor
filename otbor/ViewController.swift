62097e7ac036ce5e6430f5e54d84a280901b55c1
datukeldibek <118820830+datukeldibek@users.noreply.github.com>
2023-09-21 10:50:34 +0600

    Initial Commit

---
+++ otbor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  otbor
+//
+//  Created by Jarae on 21/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
